fd6b806d08e20d5b92ee064485a72578dd1868d4
calmgust <calmgust@gmail.com>
2018-11-07 09:33:10 +0900

    nonrepeatedCharacter

--- 02-nonrepeatedCharacter/nonrepeatedCharacter.js
+++ 02-nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,27 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+
+  var temp = {};
+
+  for(var i = 0; i < string.length; i++){
+    if(!(string[i] in temp)){
+      temp[string[i]] = 1;
+    }
+    else {
+      temp[string[i]] += 1;
+    }
+  }
+
+  if(Object.keys(temp).length === 1){
+    return null;
+  }
+
+  for(var key in temp){
+    if(temp[key] === 1){
+      return key;
+    }
+  }
+
+
 };
