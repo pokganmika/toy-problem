333bc8b9ba1a17feb221fe623ed233f12b230e05
calmgust <calmgust@gmail.com>
2018-11-29 11:28:09 +0900

    toy15

--- 15-binarySearchArray/binarySearchArray.js
+++ 15-binarySearchArray/binarySearchArray.js
@@ -9,5 +9,34 @@
  */
 
 var binarySearch = function (array, target) {
+
+  var min = 0;
+  var max = array.length - 1;
+
+  while (min <= max) {
+    var mid = Math.floor((min + max)/2);
+    if (target === array[mid]) {
+      return mid;
+    }
+    if (array[mid] <= array[max]) { 
+      if (target < array[mid] || array[max] < target) {
+        max = mid - 1;
+      } else {
+        min = mid + 1;
+
+      }
+    } else if (array[min] <= array[mid]) { 
+      if (target < array[min] || array[mid] < target) {
+        min = mid + 1;
+      } else {
+        max = mid - 1;
+
+      }
+    }
+  }
+
+  return null;
 };
 
+var index = binarySearch([1, 2, 3, 4, 5], 4);
+console.log(index); // [3]
