33c25ae93610116787d2dbac9ce92635af28942e
calmgust <calmgust@gmail.com>
2018-12-21 10:09:25 +0900

    toy24

--- 24-romanNumeralTranslator/romanNumeralTranslator.js
+++ 24-romanNumeralTranslator/romanNumeralTranslator.js
@@ -16,7 +16,7 @@
  * You should return `null` on invalid input.
  */
 
-var DIGIT_VALUES = {
+var 예시 = {
   I: 1,
   V: 5,
   X: 10,
@@ -26,7 +26,28 @@ var DIGIT_VALUES = {
   M: 1000
 };
 
-var translateRomanNumeral = function(romanNumeral){
-// TODO: Implement me!
+var translateRomanNumeral = function(로마숫자){
+  // TODO: Implement me!
+  // I, X, C, M / V, L, D
+  if (typeof 로마숫자 !== "string") {
+    return null;
+  }
+  let 결과값 = 0;
+  // 로마숫자.forEach()
+  for (let i = 0; i < 로마숫자.length; i++) {    
+    if (예시[로마숫자[i]] < 예시[로마숫자[i+1]]) {
+      const 임시값 = 예시[로마숫자[i+1]] - 예시[로마숫자[i]];
+      결과값 += 임시값;
+      i += 1;
+    } else {
+      결과값 += 예시[로마숫자[i]];
+    }
+  }
+  
+  return 결과값;
 
 };
+
+
+const result = translateRomanNumeral("CDXL");
+console.log(result);
