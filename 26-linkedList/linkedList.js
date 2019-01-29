35fef570c44545fa9d18d851813a373f7976ed6e
calmgust <calmgust@gmail.com>
2019-01-29 23:18:21 +0900

    toy26

--- 26-linkedList/linkedList.js
+++ 26-linkedList/linkedList.js
@@ -19,21 +19,44 @@
 
 var LinkedList = function(){
   //fill me in!
+  this.list = [];
+  this.head = null;
+  this.tail = null;
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+  const newNode = this.makeNode(value);
+  if(!this.list.length) {
+    this.list.push(newNode);
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    this.list.push(newNode);
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
 };
 
-LinkedList.prototype.removeHead = function(){
+LinkedList.prototype.removeHead = function () {
+  const prevHead = this.head;
+  this.head = this.head.next;
+  return prevHead.value;
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function (value) {
+  for(let i = 0; i < this.list.length; i++) {
+    if(this.list[i].value === value) {
+      return true;
+    }
+  }
+  return false;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function (value) {
+  return {
+    value,
+    next: null
+  }
 };
