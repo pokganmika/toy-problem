9de6a0a7fb564765efade8640f3a26fb6ed48a51
calmgust <calmgust@gmail.com>
2019-02-08 19:03:18 +0900

    toy31

--- 31-evenOccurrence/evenOccurrence.js
+++ 31-evenOccurrence/evenOccurrence.js
@@ -12,4 +12,25 @@
 
 var evenOccurrence = function(arr) {
   // Your code here.
+  const storage = {};
+
+  for (let i = 0; i < arr.length; i++) { 
+    if (!storage[arr[i]]) {
+      storage[arr[i]] = 1
+    } else { 
+      storage[arr[i]] += 1
+      // if (storage[arr[i]] === 2) { 
+      //   return arr[i]
+      // }
+    }
+  }
+
+  for (i = 0; i < arr.length; i++) {
+    if (storage[arr[i]] === 2) return arr[i];
+  }
+
 };
+
+// var onlyEven = evenOccurrence(['meow', 2, 1, 'meow']);
+var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
+console.log(onlyEven);
