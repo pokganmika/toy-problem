9eafc43a8a82cce08a6da47333e9c41e94f9885d
calmgust <calmgust@gmail.com>
2019-02-14 20:08:34 +0900

    toy46

--- 46-longestPalindrome/longestPalindrome.js
+++ 46-longestPalindrome/longestPalindrome.js
@@ -6,5 +6,30 @@
 * whitespace on each side of dad).
 */
 
-var longestPalindrome = function (string) {
+const longestPalindrome = string => {
+  let resultLength = 0, result = '';
+
+  for (let i = 0; i < string.length; i++) { 
+    let ascen = string.substring(i, string.length);
+
+    for (let j = ascen.length; j >= 0; j--) { 
+      let dscen = ascen.substring(0, j);
+
+      if (palCheck(dscen)) { 
+        if (dscen.length > resultLength) { 
+          result = dscen;
+          resultLength = dscen.length;
+        }
+      }
+    }
+  }
+  return result;
 };
+
+const palCheck = string => {
+  const reverse = string.split('').reverse().join('');
+  return string === reverse ? true : false;
+  // return string === reverse;
+}
+
+console.log('result : ',longestPalindrome("My dad is a racecar athlete"));
